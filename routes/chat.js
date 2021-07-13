3bb0b4085a81d96935f25fbb82cd985ad73f138e
azmul-ml <azmul.hossain@monstar-lab.com.bd>
2021-07-13 19:38:20 +0600

    Chat token refactored

--- routes/chat.js
+++ routes/chat.js
@@ -4,20 +4,17 @@ const {User} = require('../models/user');
 const asyncHandler = require('express-async-handler');
 const router = express.Router();
 
-router.post('/auth', auth, asyncHandler(async (req, res) => {
+router.post('/token', auth, asyncHandler(async (req, res) => {
   const { roles , user_id} = req.user;
-  console.log(roles, user_id);
+
   try {
     let user = await User.findOne({ _id: user_id });
     if (!user) return res.status(400).send(res.status(400).send({status: 400, message: 'Invalid User'}));
    
-    
     if(roles.length === 1) {
-      console.log('Admin');
       const token = user.adminPusherAuthToken();
       res.send(token);
     } else {
-      console.log('User');
       const token = user.userPusherAuthToken();
       res.send(token);
     }
--- startup/routes.js
+++ startup/routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
-const pusher = require('../routes/pusher');
+const chat = require('../routes/chat');
 const error = require('../middleware/error');
 
 module.exports = function(app) {
   app.use(express.json());
   app.use('/api/users', users);
   app.use('/api/auth', auth);
-  app.use('/api/pusher', pusher);
+  app.use('/api/chat', chat);
   app.use(error);
 }
